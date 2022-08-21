dee1c3052f2b4b06a6ba132f0651092998a1747a
Marcus <88283829+PyMarcus@users.noreply.github.com>
2022-08-21 17:43:20 -0300

    Adicionado funcionalidade de envio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,85 +1,76 @@
 import controllers.RepositorioController;
 import controllers.UsuarioController;
 import controllers.VersaoController;
-import ferramentas.Cores;
+import network.UpDados;
 
+/**
+ *
+ * @author CODEHUB
+ */
 public class Main {
     public static void main(String[] args) {
+        final String ANSI_YELLOW = "\u001B[33m";
         String logo = "  ____               _          _   _           _     \n" +
                 "  / ___|   ___     __| |   ___  | | | |  _   _  | |__  \n" +
                 " | |      / _ \\   / _` |  / _ \\ | |_| | | | | | | '_ \\ \n" +
                 " | |___  | (_) | | (_| | |  __/ |  _  | | |_| | | |_) |\n" +
                 "  \\____|  \\___/   \\__,_|  \\___| |_| |_|  \\__,_| |_.__/ \n" +
-                "                                                     ";
-        System.out.println(Cores.getCor("amarelo") + logo);
-        System.out.println(Cores.getCor("amarelo") + "  Digite: 'CodeHub --ajuda' para listar todos os comandos\n");
+                "                                                     " ;
+        System.out.println(ANSI_YELLOW + logo);
+        System.out.println("\033[3m  Frase de  Efeito\033[0m");
+        System.out.println(ANSI_YELLOW + "  Digite: 'CodeHub --ajuda' para listar todos os comandos\n");
 
-        // controllers
+        //controllers
         UsuarioController usuarioController = new UsuarioController();
         RepositorioController repositorioController = new RepositorioController();
-        VersaoController versaoController = new VersaoController();
-
-        // central de operacoes
+        VersaoController versao = new VersaoController();
+        UpDados enviar = new UpDados();
+        //central de operacoes
         try {
-            // verificacao de login meramente criada pra testes
+            //verificacao de login meramente criada pra testes
             if (usuarioController.estaLogado()) {
-              switch (args[0]) {
-                  case "--ajudar" -> {
-                      usuarioController.chamarAjuda();
-                  }
-                  case "--sair" -> {
-                      usuarioController.sairConta();
-                  }
-                  case "--iniciar" -> {
-                      repositorioController.iniciaRepositorio();
-                  }
-                  case "--adicionar" -> {
-                      repositorioController.adicionaAoContainer(args[1]);
-                  }
-                  case "--remover" -> {
-                      repositorioController.removeDoContainer(args[1]);
-                  }
-                  case "--versionar" -> {
-                      versaoController.versiona(args[1]);
-                  }
-                  case "--historico" -> {
-                      repositorioController.listarHistorico();
-                  }
-                  case "--registrar", "--acessar" -> {
-                      System.out.println("Usuario logado!");
-                  }
-                  case "--voltar" -> {
-                      repositorioController.voltarVersao(args[1]);
-                  }
-                  case "--apagar" -> {
-                    if (args[1].equals("-f")) repositorioController.deletaVersao(args[2]);
-                    else versaoController.deletarVersao(args[1]);
-                  }
-                  case "--container"->  {
-                    repositorioController.listaArquivosDoContainer();
-                  }
-                  case "--restaurar"->  {
-                    versaoController.recuperarVersao();
-                  }
-              }
-          } else {
-              switch (args[0]) {
-                  case "--ajudar" -> {
-                      usuarioController.chamarAjuda();
-                  }
-                  case "--registrar" -> {
-                      usuarioController.registraUsuario(null);
-                  }
-                  case "--acessar" -> {
-                      usuarioController.acessaConta();
-                  }
-                  default -> {
-                      System.out.println("Faca login antes de continuar!");
-                  }
-              }
-          }
+                switch (args[0]) {
+                    case "--ajudar" ->  {
+                        usuarioController.chamarAjuda();
+                    }
+                    case  "--sair" -> {
+                        usuarioController.sairConta();
+                    }
+                    case  "--iniciar" -> {
+                        repositorioController.iniciaRepositorio();
+                    }
+                    case  "--adicionar" -> {
+                        repositorioController.adicionaAoContainer(args[1]);
+                    }
+                    case "--registrar", "--acessar" ->  {
+                        System.out.println("Usuario logado!");
+                    }
+                    case "--versionar" -> {
+                        versao.versiona(args[1]);
+                    }
+                    case "--enviar" -> {
+                        enviar.enviarParaGitHub();
+                    }
+                }
+            } else {
+                switch (args[0]) {
+                    case "--ajudar" ->  {
+                        usuarioController.chamarAjuda();
+                    }
+                    case  "--registrar" -> {
+                        usuarioController.registraUsuario();
+                    }
+                    case  "--acessar" -> {
+                        usuarioController.acessaConta();
+                    }
+                    default -> {
+                        System.out.println("Faca login antes de continuar!");
+                    }
+                }
+            }
         } catch (ArrayIndexOutOfBoundsException e) {
             usuarioController.chamarAjuda();
         }
     }
+
 }
